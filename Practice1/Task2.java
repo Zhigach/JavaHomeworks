0ef49c9de826b8ee935955acbef77c2619dd862e
Alexander Zhigachev <sashazhig@gmail.com>
2023-04-18 08:51:28 +0300

    Task2 done

---
+++ Practice1/Task2.java
@@ -0,0 +1,34 @@
+/*
+    Вывести все простые числа от 1 до 1000
+*/
+
+public class Task2 {
+    public static void main(String[] args) {
+        printSimpleNumbers(1000);
+    }
+
+    public static boolean isPrime(int n){
+
+        if (n <= 1)
+            return false;
+
+        if (n == 2 || n == 3)
+            return true;
+
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i <= Math.sqrt(n); i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+
+        return true;
+    }
+
+    private static void printSimpleNumbers(int n){
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i))
+                System.out.print(i+"\t");
+        }
+    }
+}
