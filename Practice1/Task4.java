6e5f1fdb5f7494c47849dd495f140845b74a455e
Alexander Zhigachev <sashazhig@gmail.com>
2023-04-21 11:46:48 +0300

    Task4* done

--- Practice1/Task4.java
+++ Practice1/Task4.java
@@ -4,59 +4,108 @@
 
     Исхдим из предположения, что числа двузначные
  */
+// TODO: мне самому не нравится как выглядит решение, но из-за нехватки времени не успеваю придумать более элегантное решение.
+// хочу научится в джаву
 
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.lang.Math.pow;
 
 public class Task4{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter equation from template q + w = e, (e.g. 2? + ?5 = 69): ");
-        //String input = scanner.nextLine();
-        String testString = "2? + ?5 = 69";
-        String[] numbersStrings = testString.split(" ");
-        byte[][] numbers;
-        numbers = new byte[][] {parseStringToInt(numbersStrings[0]),
-                                parseStringToInt(numbersStrings[2]),
-                                parseStringToInt(numbersStrings[4])};
-        byte[][] variablesPositions = new byte[][]{};
+        String inputString = scanner.nextLine();
+        if (inputString == null)
+            inputString = "2? + ?3 = 6?";
+        String[] numbersStrings = inputString.split(" ");
+        int sum = 0;
+        int count = 0;
+        sum += parseStringToInt(numbersStrings[0])[0];
+        sum += parseStringToInt(numbersStrings[2])[0];
+        sum -= parseStringToInt(numbersStrings[4])[0]; // '-' is intended
+        count += parseStringToInt(numbersStrings[0])[1];
+        count += parseStringToInt(numbersStrings[2])[1];
+        count += parseStringToInt(numbersStrings[4])[1];
 
+        int[] weights = mapWeights(inputString, count);
+        byte[][] permutations = generatePermutations((byte) count);
 
+        checkMatchingNumbers(permutations, weights, sum);
     }
-    public static byte[][] findVariables(byte[] array){
-        int depth = array.length;
-        byte[] result = new byte[] {};
-        for (int i = 0; i < depth; i++) {
-            if (array[i] == -1)
 
-                result[i] = -1;
-            else
-                result[i] = Byte.parseByte(String.valueOf(numInString.charAt(i)));
+
+    private static int[] mapWeights(String equation, int numberOfWeights){
+        int[] weights = new int[numberOfWeights];
+        int negativeMultiplier = 1; // applied after the = sign is passed
+        int mapPos = 0;
+        int depth = 0;
+        char[] curNumber = new char[10];
+        int curPos = 0;
+        while (curPos < equation.length()-1){
+            while (equation.charAt(curPos) != ' '){
+                curNumber[depth++] = equation.charAt(curPos);
+                curPos++;
+                if (!(curPos < equation.length()-1))
+                    break;
+            }
+            for (int i = 0; i < depth; i++) {
+                if (curNumber[i] == '?'){
+                    weights[mapPos++] = negativeMultiplier * ((int) pow(10, (depth - i) - 1));
+                }
+            }
+            depth = 0;
+            curNumber = new char[10];
+            while (equation.charAt(curPos) == ' ' || equation.charAt(curPos) == '+' || equation.charAt(curPos) == '='){
+                if (equation.charAt(curPos) == '=')
+                    negativeMultiplier = -1;
+                curPos++;
+            }
         }
-        return ;
+        return weights;
     }
-    public static byte[] parseStringToInt(String numInString){ // -1 replaces ? - variable value
-        int depth = numInString.length();
-        byte[] result = new byte[depth];
-        for (int i = 0; i < depth; i++) {
-            if (numInString.charAt(i) == '?')
-                result[i] = -1;
-            else
-                result[i] = Byte.parseByte(String.valueOf(numInString.charAt(i)));
+
+    private static int[] parseStringToInt(String numInString){
+        int countLocal = 0;
+        int sumLocal = 0;
+        for (int i = 0; i < numInString.length(); i++) {
+            if (numInString.charAt(i) == '?') {
+                countLocal++;
+            }
+            else {
+                int value = Integer.parseInt(String.valueOf(numInString.charAt(i)));
+                sumLocal += value * pow(10, (numInString.length() - i) -1) ; // TODO учесть вес
+            }
         }
-        return result;
+        return new int[] {sumLocal, countLocal};
     }
-    private static int[] findPossibleNumbers(int[] a, int[] b, int[] result){
-
-        return new int[] {0};
+    private static void checkMatchingNumbers(byte[][] permutations, int[] weights, int sum){
+        int countMatching = 0;
+        for (int i = 0; i < permutations.length; i++) {
+            int proposedSum = sum;
+            for (int varNumber = 0; varNumber < weights.length; varNumber++) {
+                proposedSum += permutations[i][varNumber]*weights[varNumber];
+            }
+            if (proposedSum == 0){
+                System.out.println(Arrays.toString(permutations[i]) + " numbers makes equation true");
+                countMatching++;
+            }
+        }
+        if (countMatching == 0){
+            System.out.println("There is no numbers that can make this equation to be true");
+        }
     }
 
-    private static byte[][] generatePermutations(byte positionsNumber) { // честно украдено со стэковерфлоу https://stackoverflow.com/a/73374413, сам не успел осилить задачу
+    // функция честно украдено со стэковерфлоу https://stackoverflow.com/a/73374413, сам не успел осилить
+    private static byte[][] generatePermutations(byte positionsNumber) {
         //Array that holds a single permutation, e.g., {0,1,1}
-        byte[][] permutations = new byte[(int) Math.pow(10, positionsNumber)][(int) Math.pow(10, positionsNumber)];
+        int count = 0;
+        byte[][] permutations = new byte[(int) pow(10, positionsNumber)][positionsNumber];
         byte[] permutation = new byte[positionsNumber];
         do {
-            System.out.println(Arrays.toString(permutation));
+            permutations[count] = Arrays.copyOf(permutation,positionsNumber); //permutation - did not work, :monkahmmm:;
+            count++;
         } while (increaseRightMostPosition(permutation));
         return permutations;
     }
