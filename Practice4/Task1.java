bd6fd6f1fc24cc7096fd21957607c10eb7739f6d
Alexander <sashazhig@gmail.com>
2023-05-01 21:34:50 +0300

    Practice4 drafts

---
+++ Practice4/Task1.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+/*
+    Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = createRandimLinkedList(10, 0, 10);
+        System.out.printf("Initial list: %s\n",linkedList);
+        reverseLinkedList(linkedList);
+        System.out.printf("Reversed list: %s\n", linkedList);
+    }
+
+    private static LinkedList<Integer> createRandimLinkedList(int size, int lowerBound, int upperBound) {
+        Random random = new Random();
+        LinkedList<Integer> result = new LinkedList<>();
+        for (int i = 0; i < size; i++)
+            result.add(random.nextInt(lowerBound, upperBound));
+        return result;
+    }
+
+    private static void reverseLinkedList(LinkedList<Integer> linkedList) {
+        LinkedList<Integer> buffer = new LinkedList<>();
+        buffer.addAll(linkedList);
+        linkedList.clear();
+        for (int i = buffer.size() - 1; i >= 0 ; i--) {
+            linkedList.push(buffer.pop());
+        }
+    }
+}
---
+++ Practice4/Task2.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+/*
+    Реализуйте очередь с помощью LinkedList со следующими методами:
+    enqueue() - помещает элемент в конец очереди,
+    dequeue() - возвращает первый элемент из очереди и удаляет его,
+    first() - возвращает первый элемент из очереди, не удаляя.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue = createRandomQueue(10, 0, 10);
+        System.out.printf("Initial queue: %s\n", queue);
+        queue.enqueue(5);
+        System.out.printf("Enqueue demo, pushing 5: %s\n", queue);
+        System.out.printf("Dequeue demo: first queue element %s, new queue %s\n", queue.dequeue(), queue);
+        System.out.printf("First queue element: %s\n", queue.first());
+    }
+    static class Queue<E> extends LinkedList{
+
+        public void enqueue(Object item) {
+            super.add(item);
+        }
+        public Object dequeue() {
+            return super.pop();
+        }
+        public Object first() {
+            return super.getFirst();
+        }
+    }
+
+    private static Queue<Integer> createRandomQueue(int size, int lowerBound, int upperBound) {
+        Random random = new Random();
+        Queue<Integer> result = new Queue<>();
+        for (int i = 0; i < size; i++)
+            result.add(random.nextInt(lowerBound, upperBound));
+        return result;
+    }
+}
