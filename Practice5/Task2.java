c1b146c1c73c9453ab304a856499a08e2de53c9f
Alexander <sashazhig@gmail.com>
2023-05-04 08:55:23 +0300

    Practice5 drafts

---
+++ Practice5/Task2.java
@@ -0,0 +1,19 @@
+/*
+    Пусть дан список сотрудников: task2.in
+    Написать программу, которая найдёт и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности.
+ */
+
+import java.io.*;
+
+public class Task2 {
+    // cat task2.in | sort | uniq -c
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("task2.in"));
+        String line = "";
+        while (line != null) {
+            String result = br.readLine();
+        }
+
+    }
+}
