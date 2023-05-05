a26bcb97f0d99ca8f336f59b9b766522d33ec4c6
Alexander <sashazhig@gmail.com>
2023-05-05 08:50:47 +0300

    Practice5 drafts

--- Practice5/Task2.java
+++ Practice5/Task2.java
@@ -4,16 +4,27 @@
  */
 
 import java.io.*;
+import java.util.HashMap;
 
 public class Task2 {
     // cat task2.in | sort | uniq -c
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("task2.in"));
-        String line = "";
+        HashMap<String, Integer> nameCounter = new HashMap<>();
+
+        String line = br.readLine();
+        String name;
         while (line != null) {
-            String result = br.readLine();
+            name = line.split(" ")[0];
+            if (nameCounter.containsKey(name)) {
+                nameCounter.put(name, nameCounter.get(name) + 1);
+            } else {
+                nameCounter.put(name, 1);
+            }
+            line = br.readLine();
         }
 
+        System.out.println(nameCounter.toString());
     }
 }
