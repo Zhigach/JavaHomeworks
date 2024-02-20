d6ade3138197148334ab2e8444d192fb36c9c40e
Alexander <sashazhig@gmail.com>
2024-02-20 16:47:21 +0300

    fix

--- JDK/Lesson3/src/main/java/task1/Calculator.java
+++ JDK/Lesson3/src/main/java/task1/Calculator.java
@@ -17,4 +17,36 @@ public class Calculator {
             throw new IllegalArgumentException("Illegal arguments provided");
         }
     }
+
+    public static <T extends Number> T multiply(T t, T k) {
+        if (t instanceof Integer) {
+            return (T) Integer.valueOf(t.intValue() * k.intValue());
+        } else if (t instanceof Double) {
+            return (T) Double.valueOf(t.doubleValue() * k.doubleValue());
+        } else {
+            throw new IllegalArgumentException("Illegal arguments provided");
+        }
+    }
+
+    public static <T extends Number> T divide(T t, T k) {
+        if (t instanceof Integer) {
+            if (k.intValue() == 0)
+                throw new ArithmeticException("Division by zero is prohibited");
+            return (T) Integer.valueOf(t.intValue() / k.intValue());
+        } else if (t instanceof Double) {
+            return (T) Double.valueOf(t.doubleValue() / k.doubleValue());
+        } else {
+            throw new IllegalArgumentException("Illegal arguments provided");
+        }
+    }
+
+    public static <T extends Number> T substract(T t, T k) {
+        if (t instanceof Integer) {
+            return (T) Integer.valueOf(t.intValue() - k.intValue());
+        } else if (t instanceof Double) {
+            return (T) Double.valueOf(t.doubleValue() - k.doubleValue());
+        } else {
+            throw new IllegalArgumentException("Illegal arguments provided");
+        }
+    }
 }
